69f6b90974f96a64bbcaf3949bc459b02cfd0228
shringiishaan <shringiishaan@gmail.com>
2018-02-16 14:28:48 +0530

    initial project commit

---
+++ src/test/java/ispl/sample/AppSecondTest.java
@@ -0,0 +1,12 @@
+package ispl.sample;
+
+import org.testng.annotations.Test;
+
+public class AppSecondTest
+{
+    @Test
+    public void oneMoreTest() 
+	{
+        System.out.println("This is a TestNG-Maven based test");
+    }
+}
---
+++ src/test/java/ispl/sample/AppTest.java
@@ -0,0 +1,12 @@
+package ispl.sample;
+
+import org.testng.annotations.Test;
+
+public class AppTest 
+{
+    @Test
+    public void exampleOfTestNgMaven()
+	{
+        System.out.println("This is TestNG-Maven Example");
+    }
+}
